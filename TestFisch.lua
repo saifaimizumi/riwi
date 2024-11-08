bbef4dabab30c1ddd97c26ebfa356c2c839c08c6
saifaimizumi <168169720+saifaimizumi@users.noreply.github.com>
2024-11-08 13:20:16 +0700

    Update TestFisch.lua

--- TestFisch.lua
+++ TestFisch.lua
@@ -186,9 +186,6 @@ Options.MyToggle:SetValue(false)
 local Toggle = Tabs.Main:AddToggle("MyToggle", {Title = "Auto Sell", Default = false })
 
 Toggle:OnChanged(function(Value)
-        while wait() do
-            workspace.world.npcs:FindFirstChild("Marc Merchant").merchant.sellall:InvokeServer()
-        end
     end)
 Options.MyToggle:SetValue(false)
 
