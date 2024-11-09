c718dc58650e6e72430fce14849263d1b409fdd7
saifaimizumi <168169720+saifaimizumi@users.noreply.github.com>
2024-11-10 01:53:42 +0700

    Update TestFisch.lua

--- TestFisch.lua
+++ TestFisch.lua
@@ -40,6 +40,9 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 -- UI Loading
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
+local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
+
 
 -- Variables
 local selectedPlayer = ""
@@ -56,7 +59,7 @@ local Window = Fluent:CreateWindow({
     Title = "[🐟] Fisch | lyxme Hub 9 November 2024",
     SubTitle = "",
     TabWidth = 160,
-    Size = UDim2.fromOffset(580, 460),
+    Size = UDim2.fromOffset(510, 390),
     Acrylic = false,
     Theme = "Amethyst",
     MinimizeConfig = {
